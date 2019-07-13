5ec98e5d89a75d9db2d5e1625f2efb07daec8ebf
c4xp <c4xp@msn.com>
2019-07-13 13:45:49 +0300

    behat

--- demox/public/index.php
+++ demox/public/index.php
@@ -3,7 +3,7 @@ include '../cfg.php';
 include_once(APP_DIR.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'db.class.php');
 
 DB::connectPDO();
-
+echo '123';
 $rs = DB::query("SHOW DATABASES;");
 while ($r = $rs->fetch(PDO::FETCH_ASSOC)) {
     var_export($r);
